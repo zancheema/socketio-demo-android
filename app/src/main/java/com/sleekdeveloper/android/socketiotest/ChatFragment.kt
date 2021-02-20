50afee63d4a4b92f60c6337b2753473f3d04b62a
= <social.zaincheema@gmail.com>
2021-02-20 20:47:18 +0500

    added subscribe and unsubscribe events

--- app/src/main/java/com/sleekdeveloper/android/socketiotest/ChatFragment.kt
+++ app/src/main/java/com/sleekdeveloper/android/socketiotest/ChatFragment.kt
@@ -1,30 +1,76 @@
 package com.sleekdeveloper.android.socketiotest
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.gson.Gson
+import io.socket.client.IO
+import io.socket.client.Socket
+import io.socket.emitter.Emitter
 import kotlinx.android.synthetic.main.fragment_chat.*
 
 class ChatFragment : Fragment(R.layout.fragment_chat) {
+    private lateinit var chatMessagesAdapter: ChatMessagesAdapter
+    private lateinit var mSocket: Socket
+    private lateinit var userName: String
+    private lateinit var chatRoom: String
+
+    val gson = Gson()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val userName = arguments?.getString("userName")!!
-        val chatRoom = arguments?.getString("chatRoom")!!
+        userName = arguments?.getString("userName")!!
+        chatRoom = arguments?.getString("chatRoom")!!
 
-        val chatMessagesAdapter = ChatMessagesAdapter(userName)
+        chatMessagesAdapter = ChatMessagesAdapter(userName)
         chatMessages.apply {
             layoutManager = LinearLayoutManager(requireContext())
             adapter = chatMessagesAdapter
-
-            listOf(
-                ChatMessage(userName, "Hey", "chat_1"),
-                ChatMessage("otherUser", "Hello", "chat_1"),
-                ChatMessage(userName, "How's it going", "chat_1")
-            ).let {
-                chatMessagesAdapter.submitList(it)
-            }
         }
+
+        try {
+            mSocket = IO.socket("http://10.0.2.2:3000")
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Log.d("fail", "Failed to connect: ${e.message}")
+        }
+
+        mSocket.connect()
+        mSocket.on(Socket.EVENT_CONNECT, onConnect)
+        mSocket.on("newSubscriber", onNewSubscriber)
+        mSocket.on("newUnsubscriber", onNewUnsubscriber)
+    }
+
+    private val onConnect = Emitter.Listener {
+        val data = Subscriber(userName, chatRoom)
+        val jsonData = gson.toJson(data)
+        mSocket.emit("subscribe", jsonData)
+    }
+
+    private val onNewSubscriber = Emitter.Listener {
+        val name = it[0] as String
+        addToChatMessages(ChatMessage(name, "$name joined the chat room", chatRoom))
+    }
+
+    private val onNewUnsubscriber = Emitter.Listener {
+        val name = it[0] as String
+        addToChatMessages(ChatMessage(name, "$name left the chat room", chatRoom))
+    }
+
+    private fun addToChatMessages(chatMessage: ChatMessage) {
+        val tmp = ArrayList(chatMessagesAdapter.currentList)
+        tmp.add(chatMessage)
+        requireActivity().runOnUiThread {
+            chatMessagesAdapter.submitList(tmp)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        val subscriber = Subscriber(userName, chatRoom)
+        mSocket.emit("unsubscribe", gson.toJson(subscriber))
+        mSocket.disconnect()
     }
 }
---
+++ app/src/main/java/com/sleekdeveloper/android/socketiotest/Models.kt
@@ -0,0 +1,6 @@
+package com.sleekdeveloper.android.socketiotest
+
+data class Subscriber(
+    val name: String,
+    val chatRoom: String
+)
